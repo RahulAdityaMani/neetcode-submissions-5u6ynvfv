4c1263ecb07a6fcf6f408ea1e1fa49f1fef57893
Rahul Mani <rahuladityamani@hotmail.com>
2026-04-04 18:34:03 -0400

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cache = {}
+        def helper(i: int):
+            if i in cache:
+                return cache[i]
+            if i >= len(cost):
+                cache[i] = 0
+                return 0
+            cache[i] = min(cost[i] + helper(i + 2), cost[i] + helper(i + 1))
+            return cache[i]
+        return min(helper(0), helper(1))
