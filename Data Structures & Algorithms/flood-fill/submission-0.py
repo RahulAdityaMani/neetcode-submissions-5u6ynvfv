ace84cea12c2435a06e48d6b4b4c11b6a2a46906
Rahul Mani <rahuladityamani@hotmail.com>
2026-09-07 13:45:26 +0700

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        start = image[sr][sc]
+        def helper(r, c):
+            nonlocal start
+            if r < 0 or c < 0 or r >= len(image) or c >= len(image[0]) or image[r][c] != start or image[r][c] == color:
+                return
+            image[r][c] = color
+            helper(r + 1, c)
+            helper(r - 1, c)
+            helper(r, c + 1)
+            helper(r, c - 1)
+        helper(sr, sc)
+        return image
+        
