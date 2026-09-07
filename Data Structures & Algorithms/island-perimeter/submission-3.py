900fa3ee349ef2bc3d853646531cbc89b6c271d3
Rahul Mani <rahuladityamani@hotmail.com>
2026-09-07 14:31:43 +0700

    Add: island-perimeter - submission-3

---
+++ Data Structures & Algorithms/island-perimeter/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    queue = [(i, j)]
+                    grid[i][j] = 2
+                    num_edges = 0
+                    neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                    while queue:
+                        r, c = queue.pop()
+                        for nr, nc in neighbors:
+                            new_r, new_c = r + nr, c + nc
+                            if new_r < 0 or new_r >= len(grid) or new_c < 0 or new_c >= len(grid[0]) or grid[new_r][new_c] == 0:
+                                num_edges += 1
+                            elif grid[new_r][new_c] != 2:
+                                grid[new_r][new_c] = 2
+                                queue.append((new_r, new_c))
+                    return num_edges
+        return 0
+
+
